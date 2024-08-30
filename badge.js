0e3960191eacda09c29e9701f59c8b05f2df2ea0
bokkbokk <96894748+bokkbokk@users.noreply.github.com>
2024-08-30 13:34:03 +0000

    e

--- badge.js
+++ badge.js
@@ -7,7 +7,7 @@ badge.style.padding = '10px';
 badge.style.borderRadius = '5px';
 badge.style.cursor = 'pointer';
 badge.style.zIndex = '9999';
-badge.innerHTML = '<p href="index.html" style="color: black; text-decoration: none;">Home</p>';
+badge.innerHTML = '<p>Home</p>';
 badge.addEventListener('click', () => {
   badge.style.transition = 'transform 0.5s ease-in-out';
   badge.style.transform = 'scale(0.1)';
