960b463a836f0ca4e3dadb56f0a9313a782cfc1f
bokkbokk <96894748+bokkbokk@users.noreply.github.com>
2024-08-30 13:38:30 +0000

    e

--- badge.js
+++ badge.js
@@ -9,10 +9,3 @@ badge.style.cursor = 'pointer';
 badge.style.zIndex = '9999';
 badge.innerHTML = '<h1>Home</h1>';
 document.body.appendChild(badge);
-//badge.addEventListener('click', () => {
-  badge.style.transition = 'transform 0.5s ease-in-out';
-  badge.style.transform = 'scale(0.1)';
-  setTimeout(() => {
-    window.location.href = 'index.html';
-  }, 500);
-});
