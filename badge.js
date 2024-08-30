c9ef0b1de6ce712d88cd6dcbe5e0f09b1f774347
bokkbokk <96894748+bokkbokk@users.noreply.github.com>
2024-08-30 13:36:52 +0000

    badgeeee

--- badge.js
+++ badge.js
@@ -2,17 +2,17 @@ const badge = document.createElement('div');
 badge.style.position = 'fixed';
 badge.style.bottom = '20px';
 badge.style.right = '20px';
-badge.style.backgroundColor = 'rgba(255, 255, 255, 0.5)';
+badge.style.backgroundColor = 'rgba(25, 215, 25, 0.5)';
 badge.style.padding = '10px';
 badge.style.borderRadius = '5px';
 badge.style.cursor = 'pointer';
 badge.style.zIndex = '9999';
-badge.innerHTML = '<p>Home</p>';
-badge.addEventListener('click', () => {
+badge.innerHTML = '<h1>Home</h1>';
+document.body.appendChild(badge);
+//badge.addEventListener('click', () => {
   badge.style.transition = 'transform 0.5s ease-in-out';
   badge.style.transform = 'scale(0.1)';
   setTimeout(() => {
     window.location.href = 'index.html';
   }, 500);
 });
-document.body.appendChild(badge);
