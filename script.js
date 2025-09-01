614e6ca29a606cda16a10feb715faa2943ef511a
bokkbokk <tuckerwarnke@gmail.com>
2025-09-01 10:54:26 -0600

    remove please

--- script.js
+++ script.js
@@ -15,17 +15,12 @@ function getLastCommit() {
         const lastCommit = commits[0]; // The most recent commit
 
         console.log("Last Commit Info:");
-    
         console.log("SHA:", lastCommit.sha);
         let sha = lastCommit.sha;
         console.log("Message:", lastCommit.commit.message);
         let message = lastCommit.commit.message;
-        console.log("Author:", lastCommit.commit.author.name);
-        let author = lastCommit.commit.author.name;
         console.log("Date:", lastCommit.commit.author.date);
         let date = lastCommit.commit.author.date;
-        console.log("URL:", lastCommit.html_url);
-        let commitUrl = lastCommit.html_url;
         let out = date + " - " + message + " - " + sha;
         console.log(out);
         return out;
